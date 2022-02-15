820dc1e9f5fb068f5424f84ffec032c7734524fe
RohanIRathi <rathirohan8@gmail.com>
2022-02-15 14:18:02 +0530

    Created Product Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    Company = models.CharField(verbose_name='Company', max_length=50, blank=False, null=False)
+    Series = models.CharField(verbose_name='Series', max_length=50, blank=False, null=False)
+    Model = models.CharField(verbose_name='Model', max_length=50, blank=False, null=False)
+    Price = models.DecimalField(verbose_name='Price of the Model', max_digits=8, decimal_places=2, null=False, default=0.00)
+    Quantity = models.SmallIntegerField(verbose_name='Available Quantity', null=False, default=0)
+    
+    def __str__(self):
+        return str(self.Company) + " " + str(self.Series) + " " + str(self.Model)
