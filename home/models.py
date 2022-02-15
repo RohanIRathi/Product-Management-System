484be64ede4a8fd97062d660fe2d3d4333fcdcdd
RohanIRathi <rathirohan8@gmail.com>
2022-02-15 14:05:41 +0530

    Changed Representation of User Model

--- home/models.py
+++ home/models.py
@@ -5,3 +5,6 @@ class User(AbstractUser):
     CreditScore = models.IntegerField(verbose_name='Credit Score', blank=False, null=False, default=0)
     CreditLimit = models.IntegerField(verbose_name='Credit Limit', blank=True, null=True)
     Address = models.CharField(verbose_name='Address', max_length=255, null=False)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
