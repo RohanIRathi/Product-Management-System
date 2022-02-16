c0cb767cf411a7fd335ad262c479d76b2625a89a
RohanIRathi <rathirohan8@gmail.com>
2022-02-16 13:31:59 +0530

    Updated User model to point to distributor

--- home/models.py
+++ home/models.py
@@ -5,6 +5,8 @@ class User(AbstractUser):
     CreditScore = models.IntegerField(verbose_name='Credit Score', blank=False, null=False, default=0)
     CreditLimit = models.IntegerField(verbose_name='Credit Limit', blank=True, null=True)
     Address = models.CharField(verbose_name='Address', max_length=255, null=False)
+    Contact = models.BigIntegerField(verbose_name='Mobile no.', null=False, blank=False, default=1234567890)
+    Distributor = models.ForeignKey('self', null=True, verbose_name='Distributor', on_delete=models.RESTRICT)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
