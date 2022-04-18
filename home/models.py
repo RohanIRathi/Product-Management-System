f99119cce18280d8b747bc3b334564eb00c1ddff
RohanIRathi <rathirohan8@gmail.com>
2022-04-18 15:02:00 +0530

    Removed default from contact

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
 	CreditScore = models.IntegerField(verbose_name='Credit Score', blank=False, null=False, default=0)
 	CreditLimit = models.IntegerField(verbose_name='Credit Limit', blank=True, null=True)
 	Address = models.CharField(verbose_name='Address', max_length=255, null=False)
-	Contact = models.BigIntegerField(verbose_name='Mobile no.', null=False, blank=False, default=1234567890)
+	Contact = models.BigIntegerField(verbose_name='Mobile no.', null=False, blank=False)
 	Distributor = models.ForeignKey('self', verbose_name='Distributor', null=True, on_delete=models.RESTRICT)
 
 	def __str__(self):
